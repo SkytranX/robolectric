63fa902b9f73a047b673c182225749ae9d5aa1bc
Googler <noreply@google.com>
2020-05-31 16:20:50 -0700

    From parent
    PiperOrigin-RevId: 314051366

--- robolectric/src/test/java/org/robolectric/shadows/ShadowBluetoothA2dpTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowBluetoothA2dpTest.java
@@ -2,9 +2,13 @@
 package org.robolectric.shadows;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.robolectric.Shadows.shadowOf;
 
 import android.bluetooth.BluetoothA2dp;
+import android.bluetooth.BluetoothDevice;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import com.google.android.collect.Lists;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.shadow.api.Shadow;
@@ -12,13 +16,25 @@ import org.robolectric.shadow.api.Shadow;
 @RunWith(AndroidJUnit4.class)
 public class ShadowBluetoothA2dpTest {
   private static final String MOCK_MAC_ADDRESS = "00:11:22:33:AA:BB";
+  private BluetoothA2dp bluetoothA2dp;
+
+  @Before
+  public void setUp() throws Exception {
+    bluetoothA2dp = Shadow.newInstanceOf(BluetoothA2dp.class);
+  }
 
   @Test
-  public void getConnectionState_isA2dp() throws Exception {
-    BluetoothA2dp bluetoothA2dp = Shadow.newInstanceOf(BluetoothA2dp.class);
-
+  public void getConnectionState_isA2dp() {
     assertThat(
             bluetoothA2dp.getConnectionState(ShadowBluetoothDevice.newInstance(MOCK_MAC_ADDRESS)))
         .isNotNull();
+  }
+
+  @Test
+  public void getConnectedDevices_reflectsSetConnectedDevices() {
+    assertThat(bluetoothA2dp.getConnectedDevices()).isEmpty();
+    shadowOf(bluetoothA2dp)
+        .setConnectedDevices(Lists.newArrayList(Shadow.newInstanceOf(BluetoothDevice.class)));
+    assertThat(bluetoothA2dp.getConnectedDevices()).isNotEmpty();
   }
 }
--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowBluetoothA2dp.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowBluetoothA2dp.java
@@ -3,12 +3,25 @@ package org.robolectric.shadows;
 import android.bluetooth.BluetoothA2dp;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothProfile;
+import java.util.ArrayList;
+import java.util.List;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
 
 /** Shadow of {@link BluetoothA2dp}. */
 @Implements(BluetoothA2dp.class)
 public class ShadowBluetoothA2dp {
+  private List<BluetoothDevice> bluetoothDevices = new ArrayList<>();
+
+  public void setConnectedDevices(List<BluetoothDevice> bluetoothDevices) {
+    this.bluetoothDevices = bluetoothDevices;
+  }
+
+  @Implementation
+  protected List<BluetoothDevice> getConnectedDevices() {
+    return bluetoothDevices;
+  }
+
   @Implementation
   protected int getConnectionState(BluetoothDevice device) {
     return BluetoothProfile.A2DP;
